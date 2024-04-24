f2565c423f248fb97de4be4ff1a538f0e49b0742
Biella <eduardo.biella@gmail.com>
2024-04-24 20:50:42 -0300

    Biblioteca Keras
    Para reconhecimento facial e de outras imagens

--- ESP32/Reconhecimento de Imagem/RI.py
+++ ESP32/Reconhecimento de Imagem/RI.py
@@ -4,3 +4,9 @@ import sklearn
 import matplotlib.pyplot as pd
 import warnings
 
+#4 opções de bibliotecas para reconhecimento de imagens em python #https://ealexbarros.medium.com/4-melhores-bibliotecas-em-python-para-reconhecimento-de-imagens-8c1f97ead6e4
+
+# import OpenCV
+# import TensorFlow
+# import PyTorch
+import Keras
